81384401db4833cf83a5d4240d12472df2206014
Michael Kropat <mail@michael.kropat.name>
2016-05-30 10:55:32 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,81 @@
+module.exports = {
+    stockProfileName: 'stock',
+    profiles: [
+        'stock',
+        //'disconnect',
+        //'ghostery',
+        //'privacy-badger',
+        //'ublock-origin',
+    ],
+    suspects: [
+        'facebook',
+        'google',
+        'twitter'
+    ],
+    sites: [
+        {
+            homeUrl: 'http://arstechnica.com/',
+            articleSelector: 'a.feature-container'
+        },
+        //{
+        //    homeUrl: 'http://www.bbc.com/',
+        //    articleSelector: '.media-list a.media__link'
+        //},
+        //{
+        //    homeUrl: 'https://www.buzzfeed.com/',
+        //    articleSelector: 'a.lede__link'
+        //},
+        //{
+        //    homeUrl: 'http://www.cnn.com/',
+        //    articleSelector: 'article a'
+        //},
+        //{
+        //    homeUrl: 'http://espn.go.com/',
+        //    articleSelector: 'a.realStory'
+        //},
+        //{
+        //    homeUrl: 'http://gawker.com/',
+        //    articleSelector: '.featured-item a'
+        //},
+        //{
+        //    homeUrl: 'http://gizmodo.com/',
+        //    articleSelector: '.headline a'
+        //},
+        //{
+        //    homeUrl: 'http://kotaku.com/',
+        //    articleSelector: 'a.featured-link'
+        //},
+        //{
+        //    homeUrl: 'http://www.latimes.com/',
+        //    articleSelector: 'article h2 a'
+        //},
+        //{
+        //    homeUrl: 'http://www.nytimes.com/',
+        //    articleSelector: '.story-heading a'
+        //},
+        //{
+        //    homeUrl: 'http://www.theregister.co.uk/',
+        //    articleSelector: 'a.large_story'
+        //},
+        //{
+        //    homeUrl: 'http://www.reuters.com/',
+        //    articleSelector: '.story-title a'
+        //},
+        //{
+        //    homeUrl: 'http://www.slate.com/',
+        //    articleSelector: 'a.primary'
+        //},
+        //{
+        //    homeUrl: 'http://www.techrepublic.com/',
+        //    articleSelector: '.title a'
+        //},
+        //{
+        //    homeUrl: 'https://www.washingtonpost.com/',
+        //    articleSelector: '.headline a'
+        //},
+        //{
+        //    homeUrl: 'http://www.wsj.com/',
+        //    articleSelector: 'a.wsj-headline-link'
+        //}
+    ]
+};
